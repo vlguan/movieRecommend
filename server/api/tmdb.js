03983ff6fcba5383c1088fec8be7f62877ba76ef
vlguan <68829739+vlguan@users.noreply.github.com>
2023-11-19 15:19:30 -0800

    api call search created and working
    Changed erroring system and .env configuration to pass tests

--- server/api/tmdb.js
+++ server/api/tmdb.js
@@ -1,3 +1,4 @@
+require('dotenv').config( { path: "./config.env"})
 const apiKey = process.env.API_KEY;
 const BASEURL = process.env.BASEURL;
 
@@ -5,25 +6,23 @@ async function movieQuery(query) {
   try {
     const encodedQuery = encodeURIComponent(query);
     // query should be preencoded before appending to the URL
-    const url = `${BASEURL}/search/movie?query=${encodedQuery}`;
+    const url = `${BASEURL}search/movie?query=${encodedQuery}`;
     const headers = {
       accept: 'application/json',
       Authorization: `Bearer ${apiKey}`
     };
 
-    const response = await fetch(url, { headers });
-
-    if (!response.ok) {
+    const response = await fetch(url, { options: 'GET', headers });
+    const data = await response.json();
+    console.log(response);
+    if (response.ok == false){
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
-
-    const data = await response.json();
-
-    console.log(data);
+    return data;
   } catch (error) {
-    console.error('Error:', error);
+    console.log("error", error);
+    throw new Error( `${error.message}`);
   }
 }
 
-
-exports.movieQuery = movieQuery;
+module.exports = { movieQuery };
