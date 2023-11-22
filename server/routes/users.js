34a976e1b9353ebca9327557a0aa702d75b5ab44
Vince Guan <vinceguan@gmail.com>
2023-11-22 15:01:47 -0800

    changed user routes to use model

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,7 +1,8 @@
 // src/routes/userRoutes.js
+// This file are
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User');
+const User = require('../models/usersModel');
 const userRoutes = express.Router();
 // This will help us connect to the database
 const dbo = require('../db/conn');
@@ -9,32 +10,33 @@ const encryptServ = require('../api/encryptionService');
 // This helps convert the id from string to ObjectId for the _id.
 const ObjectId = require("mongodb").ObjectId;
 // Create a user
-userRoutes.route('/user/register').post(function (req, response) {
-  let db_connect = dbo.getDb();
-  //encrypts password
+userRoutes.route('/user/register').post(async function (req, response) {
+  try{
+    //encrypts password
   let password = encryptServ.hashPassword(req.body.password)
   let myquery = {username: ObjectId(req.body.username)}
   // checks if username already exists
-  let result = db_connect.collection("users").findone(myquery);
+  let result = User.findOne(myquery);
   if (result == null || result == undefined){
     let userObj = {
       username: req.body.username,
       password,
       email: req.body.email
     };
-    db_connect.collection("users").insertOne(userObj, function (err, res) {
-      if (err) throw err;
-      response.json(res);
-    });
-  }else{
-    throw new Error('Username Already Exists')
-  }
- });
-
+    const response = await User.saveUser(userObj);
+    res.status(200).json(response)
+    }else{
+      throw new Error('Username exists')
+    }
+  }catch(error){
+    res.status(500).json({ error: error.message });
+    }
+  });
+  
 // Push recent search to history
 userRoutes.route('/user/history').post(function (req, res){
   try {
-    let myquery = { _id: ObjectId(req.body.id) };
+    User.addSearchToHistory(username, req.body.searchTerm)
     res.status(200).json(users);
   } catch (error) {
     res.status(500).json({ error: error.message });
