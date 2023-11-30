ef5031280040ae68ced871eb5324cee7b469072b
Vince Guan <vinceguan@gmail.com>
2023-11-30 13:37:59 -0800

    added navbar and search

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,19 @@
 import logo from './logo.svg';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 import LoginForm from './components/login';
 import UserRegistrationForm from './components/registration';
+import Navbar from "./components/navbar";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <LoginForm/>
-        <UserRegistrationForm/>
-      </header>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path='/login' element={<LoginForm/>}/>
+        <Route path='/login/register' element={<UserRegistrationForm/>}/>
+      </Routes>
     </div>
   );
 }
